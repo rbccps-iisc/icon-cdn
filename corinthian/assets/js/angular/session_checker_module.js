9e9b798e5cd96b40ce674fa6575b04184786ffe2
Jishnu P <jishnu.jayakumar182@gmail.com>
2019-02-27 11:49:08 +0530

    Update session_checker_module.js

--- corinthian/assets/js/angular/session_checker_module.js
+++ corinthian/assets/js/angular/session_checker_module.js
@@ -7,11 +7,9 @@ session_checker.controller('session_checkerCtrl', ['$scope', function($scope) {
     	var apikey = sessionStorage.getItem('apikey')
 
     	if(id===null || apikey===null){
-    		if(window.location.href != location.origin + "/ui/pages/login")
+    		if(window.location.href !== location.origin + "/ui/pages/login")
 			window.location = location.origin + "/ui/pages/login";
-			console.log("IF", window.location.href != location.origin + "/ui/pages/login", id===null || apikey===null)
 		}else{
-			console.log("ELSE", window.location.href != location.origin + "/ui/pages/login", id===null || apikey===null)
 			if (window.location.href === location.origin + "/ui/pages/login/"){
 				var _id = sessionStorage.getItem('id')
 				if(_id=='admin'){
